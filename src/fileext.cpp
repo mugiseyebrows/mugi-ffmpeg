6dd916ab29bb910a062f0b50a02e5db41a3c4821
Doronin Stanislav <mugisbrows@gmail.com>
2019-11-06 14:31:03 +0300

    video audio subtitles exts

--- src/fileext.cpp
+++ src/fileext.cpp
@@ -4,17 +4,22 @@
 
 QStringList FileExt::videoExts()
 {
-    return {".mkv",".mp4",".ts",".avi",".wmv",".mpg"};
+    return {".avi", ".vfw", ".divx", ".mpg", ".mpeg", ".m1v", ".m2v", ".mpv", ".dv", ".3gp",
+        ".mov", ".mp4", ".m4v", ".mqv", ".dat", ".vcd", ".ogg", ".ogm", ".ogv", ".ogx",
+        ".asf", ".wmv", ".bin", ".iso", ".vob", ".mkv", ".nsv", ".ram", ".flv", ".rm",
+        ".swf", ".ts", ".rmvb", ".dvr-ms", ".m2t", ".m2ts", ".mts", ".rec", ".wtv",
+        ".f4v", ".hdmov", ".webm", ".vp8", ".bik", ".smk", ".m4b", ".wtv", ".part"};
 }
 
 QStringList FileExt::audioExts()
 {
-    return {".mka",".mp3",".flac",".aac",".ac3"};
+    return {".mp2", ".mp3", ".mpc", ".ogg", ".oga", ".wav", ".wma", ".aac", ".ac3", ".dts",
+        ".ra", ".ape", ".flac", ".thd", ".mka", ".m4a", ".wv", ".shn", ".opus"};
 }
 
 QStringList FileExt::subtitlesExts()
 {
-    return {".srt",".ass",".ssa"};
+    return {".srt", ".sub", ".ssa", ".ass", ".idx", ".txt", ".smi", ".rt", ".utf", ".aqt", ".vtt"};
 }
 
 bool FileExt::isVideo(const QString &path)
